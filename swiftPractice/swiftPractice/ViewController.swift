f624c308af2967facc881ea2fd160f1469108551
김현우 <HyeonwooKim@gimhyeon-uui-macbook-pro.local>
2015-12-04 16:52:51 +0900

    이미지 밑에 텍스트 추가 & 숨김

--- swiftPractice/swiftPractice/ViewController.swift
+++ swiftPractice/swiftPractice/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Show text "recording in progress"
+        //TODO: Record the user's voice
+        print("in recordAudio")
+    }
 
 }
 
