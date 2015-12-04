25db27449b799197b9cedb5963806e3ec40c43df
yoonjong92 <yoonjong@theventures.co>
2015-12-04 12:05:48 +0900

    Practice project create

---
+++ swiftPractice/swiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftPractice
+//
+//  Created by 박윤종 on 2015. 12. 4..
+//  Copyright © 2015년 박윤종. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
