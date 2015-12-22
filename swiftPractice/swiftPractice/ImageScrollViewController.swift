cf03615e309eb6f1bfa05bb396de9a6d53833c56
KimHyeonwoo <woo960131@tastelog.net>
2015-12-22 17:12:25 +0900

    스크롤뷰 1번 구현

---
+++ swiftPractice/swiftPractice/ImageScrollViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ImageScrollViewController.swift
+//  swiftPractice
+//
+//  Created by 김현우 on 2015. 12. 22..
+//  Copyright © 2015년 박윤종. All rights reserved.
+//
+
+import UIKit
+
+class ImageScrollViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView: UIScrollView!
+    
+    var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let image = UIImage(named: "photo1.png")!
+        imageView = UIImageView(image: image)
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
+        scrollView.addSubview(imageView)
+        
+        scrollView.contentSize = image.size
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        let scrollViewFrame = CGRectMake(0, 0, self.view.frame.size.width,self.view.frame.size.height - UIApplication.sharedApplication().statusBarFrame.height - (self.navigationController?.navigationBar.frame.height)!)  // scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight)
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = minScale
+        scrollView.setZoomScale(minScale, animated: false)
+        centerScrollViewContents()
+    }
+    
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
+    }
+    
+    func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+        let pointInView = recognizer.locationInView(imageView)
+        
+        var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+        
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectToZoomTo = CGRectMake(x, y, w, h)
+        
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
